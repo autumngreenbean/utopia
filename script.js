8fdde15b9747222ade9752f0517d2618bec106d3
Autumn Jing Green <67246658+autumngreenbean@users.noreply.github.com>
2025-03-15 19:13:13 -0700

    cleanup

--- script.js
+++ script.js
@@ -5,40 +5,35 @@ import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/
 const scene = new THREE.Scene(), camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('canvas'), antialias: true });
 renderer.setSize(innerWidth, innerHeight);
-renderer.setClearColor(0xFFFFFF, 1);  // Set the background to white (or any color you prefer)
+renderer.setClearColor(0xFFFFFF, 1);  
 document.body.appendChild(renderer.domElement);
 
-// Lighting
 const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(5, 5, 5);
 scene.add(light);
-// Load GLB model
+
 let model; 
-new GLTFLoader().load('scene.gltf', gltf => {
+new GLTFLoader().load('nail_charms_XD/scene.gltf', gltf => {
     model = gltf.scene;
 
-    // Set scale, position, and rotation for the model
     model.scale.set(30, 30, 30);  
     model.position.set(0, 0, 0); 
-    model.rotateX(Math.PI / 7);
     
-    // Traverse through all the meshes in the model and set wireframe
-    model.traverse(child => {
+    // model.rotateX(Math.PI / 7);
+        model.traverse(child => {
         if (child.isMesh) {
-            child.material.wireframe = true; // Enable wireframe for each mesh
+            child.material.wireframe = false; // Enable wireframe for each mesh
         }
     });
-
     scene.add(model);
 });
 
-// Camera setup
-camera.position.set(0, 1, 150); // Move camera back to see both objects
-camera.lookAt(0, 0, 0);
+camera.position.set(0, 100, 150); //x, y, z
+camera.lookAt(0, 0, 0); 
 
 (function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    model.rotation.z += 0.01;  // Adjust the value for speed
+    // model.rotation.z += 0.01; 
 
 })();
